67864174139415c6d325dd4560d5f6adba24d53d
Violeta Ai <violetaai.naharrozaldivar@alum.uca.es>
2024-11-13 12:20:56 +0100

    ultimo commit primera parte

--- Git_IA/src/jungle/almacen_cli.java
+++ Git_IA/src/jungle/almacen_cli.java
@@ -5,10 +5,8 @@ import java.util.Vector;
 public class Almacen_cli 
 { 
  private Vector<Cliente> vec = new Vector<Cliente>(); 
- private Cliente c1 = new Cliente("32074097L","Don Bartolome Mendez 
- Zuloaga","01/04/1998","H","B+"); 
- private Cliente c2 = new Cliente("32094058k","Cristiano Ronaldo Dos Santos 
- Aveiro","02/04/1999","H","A-"); 
+ private Cliente c1 = new Cliente("32074097L","Don Bartolome Mendez  Zuloaga","01/04/1998","H","B+"); 
+ private Cliente c2 = new Cliente("32094058k","Cristiano Ronaldo Dos Santos Aveiro","02/04/1999","H","A-"); 
  public Almacen_cli() 
  { 
   this.vec.add(c1); 
---
+++ README_segunda parte.md
@@ -0,0 +1,7 @@
+## PUNTO 13
+### Unión de dos nuevos alumnos
+Alumno 1 -> Marta Jones González
+Alumno 2 -> Pablo del Valle Davoodzadeh
+Alumno 3 -> Ezequiel Lemos Cárdenas
+Alumno 4 -> Violeta Ai Naharro Zaldívar
+
